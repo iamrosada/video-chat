a4b46ab9ea9f7e167621683796a6cfbdc972d76d
iamrosada <luisrosada2018@gmail.com>
2022-09-25 14:37:06 +0300

    created the PORT for listening the server

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const server = require("http").createServer(app);
 const cors = require("cors");
 
 const io = require("socket.io")(server, {
-    cors: {
-        origin: "*",
-        methods: ["GET", " POST"],
-    },
+  cors: {
+    origin: "*",
+    methods: ["GET", " POST"],
+  },
 });
 
 app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("server is listening");
+});
